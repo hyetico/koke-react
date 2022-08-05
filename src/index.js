632710d5e748a2de97c364ed2ba526628db8d29f
KHK <rmadm7s@naver.com>
2022-08-05 11:33:14 +0900

    work

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename='/kokereact'>
+      <BrowserRouter basename='/kokereacta'>
         <App />
       </BrowserRouter>
     </Provider>
